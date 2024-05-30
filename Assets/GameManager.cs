12e97b5447da3b2415ce0c744956a289ac55d48b
FranPonce3D <117677911+FranPonce3D@users.noreply.github.com>
2024-05-30 09:38:25 +0200

    Corrección bug Game Manager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-private void Start(){}
-	public class GameManager : MonoBehaviour(){
+
+	public class GameManager : MonoBehaviour{
     // Start is called before the first frame update
     #region Singleton;
 	public static GameManager Instance;
-	private void Awake()
-		if ( Instance==null) Instance=this;{
-			
+	private void Awake() {
+		if (Instance == null)
+		{
+			Instance = this;
 		}
+	}
+		
 	#endregion;
-	public float currentScore=Of;
+	public float currentScore=0f;
 	
 	public bool isPlaying =false;
 	private void Update(){
@@ -22,6 +25,7 @@ private void Start(){}
 	
 	public void GameOver(){
 		currentScore=0;
+		isPlaying = false;
 	}
 	public string PrettyScore (){
 		return Mathf.RoundToInt(currentScore).ToString();
